eefa9cfe20f293be970b019c3df5580fc007ec78
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-03-12 19:08:26 -0300

    Adicionado estilo aos cards

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <div className='flex-col md:flex-row flex  justify-between pb-3 bg-slate-50 border-b-2 border-slate-100'>
         <div className="w-80 rounded-full overflow-hidden  mt-4 ml-5 border-4 border-yellow-700 shadow-md self-center"><img src={Comida_1} /></div>
         <div className="  self-center">
-          <h1 className="  text-6xl text-yellow-300  mt-10 md:mt-0 animate-bounce">Verde Brisa</h1>
+          <h1 className="  text-6xl text-yellow-300  mt-10 md:mt-0 ">Verde Brisa</h1>
           <h2 className=" text-2xl ml-10 mb-12 md:mb-0">Melhor restaurante de família</h2>
         </div>
         <div className=" self-center"> 
