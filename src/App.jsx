37e3443aa2703a07433cbf4819daacbe94d0bbe0
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-03-12 19:53:16 -0300

    Adicionado history já estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import History from './layout/history'
 import Menu from './layout/menu'
 import Navbar from './layout/navbar'
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div>
       <Navbar/>
       <Menu/>
-  
+      <History/>
     </div>
   )
 }
---
+++ src/layout/history.jsx
@@ -0,0 +1,15 @@
+
+import styles from "./history.module.css"
+function History() {
+    return ( 
+        <div className={styles.conteiner}>
+            <div className="bg-black text-white opacity-90 py-4">
+            <h1 className="text-3xl ml-2 mb-5 mt-2 font-bold opacity-100 text-center ">Nossa história</h1>
+            <p className="opacity-100">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem praesentium rem possimus illum adipisci provident reprehenderit maxime ea pariatur repellat nam deserunt, dolorum voluptatibus illo, temporibus sint, explicabo numquam quae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et animi illum eaque nulla praesentium quos vitae cupiditate, laborum perferendis totam ullam, accusamus minus reprehenderit! Officia at sed quibusdam dolore suscipit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus temporibus officiis reprehenderit impedit! Tempore assumenda dolorem velit, tempora ad illo corrupti optio totam nobis perspiciatis laboriosam earum beatae aut maiores!</p>
+            </div>
+            
+        </div>
+     );
+}
+
+export default History;
