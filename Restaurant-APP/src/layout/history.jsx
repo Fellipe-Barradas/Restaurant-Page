56b691f04e773a5c0bb9f342b591839607e43b27
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-03-13 14:39:13 -0300

    Corrigindo parágrafos do about

--- Restaurant-APP/src/layout/history.jsx
+++ Restaurant-APP/src/layout/history.jsx
@@ -5,7 +5,7 @@ function History() {
         <div className={styles.conteiner}>
             <div className="bg-black text-white opacity-90 py-4">
             <h1 className="text-3xl ml-2 mb-5 mt-2 font-bold opacity-100 text-center ">Nossa história</h1>
-            <p className="opacity-100">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem praesentium rem possimus illum adipisci provident reprehenderit maxime ea pariatur repellat nam deserunt, dolorum voluptatibus illo, temporibus sint, explicabo numquam quae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et animi illum eaque nulla praesentium quos vitae cupiditate, laborum perferendis totam ullam, accusamus minus reprehenderit! Officia at sed quibusdam dolore suscipit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus temporibus officiis reprehenderit impedit! Tempore assumenda dolorem velit, tempora ad illo corrupti optio totam nobis perspiciatis laboriosam earum beatae aut maiores!</p>
+            <p className="text-lg p-4 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem praesentium rem possimus illum adipisci provident reprehenderit maxime ea pariatur repellat nam deserunt, dolorum voluptatibus illo, temporibus sint, explicabo numquam quae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et animi illum eaque nulla praesentium quos vitae cupiditate, laborum perferendis totam ullam, accusamus minus reprehenderit! Officia at sed quibusdam dolore suscipit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus temporibus officiis reprehenderit impedit! Tempore assumenda dolorem velit, tempora ad illo corrupti optio totam nobis perspiciatis laboriosam earum beatae aut maiores!</p>
             </div>
             
         </div>
