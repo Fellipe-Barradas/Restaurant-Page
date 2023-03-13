cbd1db35ca6a54930cf8e3891703a25867ad59eb
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-03-13 14:45:18 -0300

    Melhorando a responsividade do menu

--- Restaurant-APP/src/layout/menu.jsx
+++ Restaurant-APP/src/layout/menu.jsx
@@ -8,13 +8,15 @@ import Comida_4 from "../imgs/comida_4.jpg"
 function Menu() {
     return (
         <div className="mb-10 bg-gray-100 py-10 shadow-md">
-        <div className="flex-col ">
+        <div className="flex-col  justify-center align-middle w-full">
             <h1 className="mt-10 mb-3 border-b-2 text-3xl ml-4 w-52 text-yellow-300">Mais pedidos</h1>
-            <div className=" grid grid-cols-1  gap-4 sm:grid-cols-2 mt-10 lg:grid-cols-4 ml-36 sm:ml-12 md:ml-4">
-            <CardFood name='Tapioca' image={Comida_1} cost='20,00'/>
-            <CardFood name='Paçoca' especial='true' image={Comida_2} cost='15,99'/>
-            <CardFood name='Vinagrete'  image={Comida_3} cost='20,99'/>
-            <CardFood name='Macaxeira' especial='true' image={Comida_4} cost='19,99'/>
+            <div className="flex justify-center">
+              <div className=" grid grid-cols-1  gap-4 sm:grid-cols-2 mt-10 lg:grid-cols-4">
+                <CardFood name='Tapioca' image={Comida_1} cost='20,00'/>
+                <CardFood name='Paçoca' especial='true' image={Comida_2} cost='15,99'/>
+                <CardFood name='Vinagrete'  image={Comida_3} cost='20,99'/>
+                <CardFood name='Macaxeira' especial='true' image={Comida_4} cost='19,99'/>
+              </div> 
             </div>
         </div>
         <button className="bg-gray-200 p-2 rounded-full absolute right-1/2 animate-bounce"
